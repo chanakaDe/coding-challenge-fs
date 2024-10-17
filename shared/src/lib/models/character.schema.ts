e0af9e44c000c46cc3afa77bd64d2bfae6275ae4
chanu1993 <chanu1993@gmail.com>
2024-10-17 12:38:10 +0200

    added descriptive comments.

--- shared/src/lib/models/character.schema.ts
+++ shared/src/lib/models/character.schema.ts
@@ -1,11 +1,18 @@
 import { z } from 'zod';
 
+// Define the schema for a Star Wars character
 export const CharacterSchema = z.object({
+  // Unique identifier for the character
   uid: z.string(),
+  // Name of the character
   name: z.string(),
+  // Birth year of the character 
   birth_year: z.string(),
+  // Name of the character's home planet
   homeworld: z.string(),
+  // Terrain type which is the Aggregated value of the character's homeworld
   terrain: z.string(),
 });
 
+// Create a TypeScript type based on the CharacterSchema which can be accessed in the project.
 export type Character = z.infer<typeof CharacterSchema>;
